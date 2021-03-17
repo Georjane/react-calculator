043cb2e9a8dea594e436510e7390592cb9839b14
Georjane <pzgeorjane@gmail.com>
2021-03-17 21:32:43 +0100

    Implent display component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,7 @@
-import PropTypes from 'prop-types';
-
 const Button = function (props) {
   return (
     <div className="Button">Button Component</div>
   );
 };
-
-Button.PropTypes = {
-  result: PropTypes.string
-}
-
-Button.defaultProps = {
-  result: '0'
-}
 
 export default Button;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,21 @@
-const Display = function () {
+import PropTypes from 'prop-types';
+
+const Display = function (props) {
   return (
-    <div className="Display">Display Component</div>
+    <div className="Display">
+      Display Component has result property:
+      $
+      {props.result}
+    </div>
   );
+};
+
+Display.PropTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
 };
 
 export default Display;
