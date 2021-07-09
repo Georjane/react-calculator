3d8c911fd5e54aac6d611b3e5268b96ad713fe53
Georjane <pzgeorjane@gmail.com>
2021-07-09 13:18:23 +0100

    Fix decimal point issues

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -4,20 +4,20 @@ const operate = (numberOne, numberTwo, operation) => {
   let result;
   const num1 = new Big(numberOne);
   const num2 = new Big(numberTwo);
-  if (operation === '-') {
-    result = num1.minus(num2);
-  } else if (operation === '+') {
+  if (operation === '+') {
     result = num1.plus(num2);
+  } else if (operation === '-') {
+    result = num1 - num2;
   } else if (operation === 'x') {
-    result = num1.times(num2);
+    result = num1 * num2;
   } else if (operation === '÷') {
-    if (numberTwo !== '0') {
-      result = num1.div(num2);
-    } else {
-      return 'Division by zero Error';
+    if (num2 !== '0') {
+      result = num1 / num2;
+    } else if (num2 === '0') {
+      result = 'Division by zero Error';
     }
   } else if (operation === '%') {
-    result = num1.div(100);
+    result = num1 / 100;
   }
   return result.toString();
 };
