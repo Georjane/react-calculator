d35f188760991ae5c8a26c6c1009187390360c74
Georjane <pzgeorjane@gmail.com>
2021-07-10 08:51:49 +0100

    Change stateful App component to func component using hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-      totalStatus: true,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+const App = () => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
-  handleClick = buttonName => {
-    const {
-      total, next, operation, totalStatus,
-    } = this.state;
+  // useEffect(() => {
+  const handleClick = buttonName => {
     const result = calculate({
-      total, next, operation, totalStatus,
+      total, next, operation,
     }, buttonName);
-    this.setState({
-      total: result.total,
-      next: result.next,
-      operation: result.operation,
-      totalStatus: result.totalStatus,
-    });
-  }
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
+  };
+  // });
+  return (
+    <>
+      <Display result={total} next={next} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <>
-        <Display result={total} next={next} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+export default App;
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//       totalStatus: true,
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick = buttonName => {
+//     const {
+//       total, next, operation, totalStatus,
+//     } = this.state;
+//     const result = calculate({
+//       total, next, operation, totalStatus,
+//     }, buttonName);
+//     this.setState({
+//       total: result.total,
+//       next: result.next,
+//       operation: result.operation,
+//       totalStatus: result.totalStatus,
+//     });
+//   }
+
+//   render() {
+//     const { total, next } = this.state;
+//     return (
+//       <>
+//         <Display result={total} next={next} />
+//         <ButtonPanel clickHandler={this.handleClick} />
+//       </>
+//     );
+//   }
+// }
